257429d35a5897606025ede45b3fd93fccf901d8
opablohenrique <ps569022@gmail.com>
2024-02-19 14:53:16 -0300

    created theme light

--- scrip.js
+++ scrip.js
@@ -1,4 +1,30 @@
-function light() {
+
+theme.addEventListener("change", function (){
+    var checkbox = document.getElementById ("theme");
     var body = document.getElementById("body");
-    body.style.backgroundColor = 'white'
-}
+    
+    if (checkbox.checked == true){
+        body.style.backgroundColor = '#dadada'
+        document.documentElement.style.setProperty ('--textcolor', '#22222')
+        document.documentElement.style.setProperty ('--secondary-color', '#686767')
+
+    }else {
+        body.style.backgroundColor = '#222222'
+        document.documentElement.style.setProperty ('--textcolor', '#fff')
+        document.documentElement.style.setProperty ('--secondary-color', '#B4B4B4')
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
